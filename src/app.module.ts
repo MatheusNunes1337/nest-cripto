3ce503c9f54b48eaa546bf0658d8296265076d3a
Matheus Nunes <matheus.silva@compasso.com.br>
2022-01-14 16:59:09 -0300

    feat: add JoiPipeModule in imports field of app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './database/database.module';
+import { JoiPipeModule } from 'nestjs-joi';
 
 @Module({
-  imports: [DatabaseModule, WalletModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), WalletModule, JoiPipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
