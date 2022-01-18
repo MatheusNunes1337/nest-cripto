2a7abe88957b4ac86af1d571590277c8c97505ec
Matheus Nunes <matheus.silva@compasso.com.br>
2022-01-18 10:04:08 -0300

    feat: add feature to add coins to wallet in update method of wallet

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   ConflictException,
   Injectable,
   NotFoundException,
@@ -12,6 +13,8 @@ import { SearchWalletDto } from './dto/search-wallet.dto';
 import { Wallet } from './entities/wallet.entity';
 import Coin from './entities/coin.entity';
 import { CoinDto } from './dto/coin.dto';
+import { AddressDto } from './dto/address.dto';
+import { convertCoin } from 'src/utils/convertCoin';
 
 @Injectable()
 export class WalletService {
@@ -33,32 +36,54 @@ export class WalletService {
     return this.walletRepository.find(filter);
   }
 
-  async findByAddress(address: string): Promise<WalletDto> {
-    const wallet = await this.walletRepository.findOne({ address });
+  async findByAddress(address: AddressDto): Promise<Wallet> {
+    const wallet = await this.walletRepository.findOne(address);
     if (!wallet) throw new NotFoundException('Wallet not found');
 
     return wallet;
   }
 
-  async update(address: string, coinDto: Array<CoinDto>) {
+  async update(address: AddressDto, coinDto: CoinDto[]) {
     const result = await this.findByAddress(address);
     if (!result) throw new NotFoundException('Wallet not found');
 
     await Promise.all(
       coinDto.map(async (coin) => {
         const wallet = await this.coinRepository.findOne({
-          where: { wallet: address, currentCoin: coin.currentCoin },
+          where: { wallet: address, coin: coin.quoteTo },
         });
-        coin.wallet.address = address;
-        if (wallet) return this.coinRepository.update(wallet.id, coin);
-        else return this.coinRepository.save(coin);
+
+        const response = await convertCoin(coin.quoteTo, coin.currentCoin);
+        const coinFullname = response.name.split('/')[1];
+
+        const payload = {
+          coin: response.codein,
+          fullname: coinFullname,
+          amount: null,
+          wallet: result,
+        };
+
+        if (wallet) {
+          payload.amount = wallet.amount + response.ask * coin.value;
+          if (payload.amount < 0)
+            throw new BadRequestException(
+              'It seems like you do not have enough balance',
+            );
+          return this.coinRepository.update(wallet.id, payload);
+        } else {
+          if (coin.value < 0)
+            throw new BadRequestException(
+              'You cannot add a coin into your wallet with negative amount',
+            );
+          payload.amount = response.ask * coin.value;
+          return this.coinRepository.save(payload);
+        }
       }),
     );
   }
 
-  async remove(address: string): Promise<Record<never, string>> {
-    console.log(address);
-    const wallet = await this.findByAddress(address);
+  async remove(address: AddressDto): Promise<Record<never, string>> {
+    const wallet = await this.walletRepository.findOne(address);
     if (!wallet) throw new NotFoundException('Wallet not found');
 
     return this.walletRepository.delete(address);
