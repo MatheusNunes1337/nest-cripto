d54d736065d7c247929a15df367a344e65bb3c4c
Matheus Nunes <matheus.silva@compasso.com.br>
2022-01-19 14:41:13 -0300

    feat: import WalletSerialize in wallet module

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -4,10 +4,11 @@ import { WalletController } from './wallet.controller';
 import { Wallet } from './entities/wallet.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Coin from './entities/coin.entity';
+import { WalletSerialize } from './wallet.serialize';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Wallet, Coin])],
   controllers: [WalletController],
-  providers: [WalletService],
+  providers: [WalletService, WalletSerialize],
 })
 export class WalletModule {}
